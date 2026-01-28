2793d73dbab653aa01bc70db1928b0ca6c5e6de7
sumit <1920sumitpandey@gmail.com>
2026-01-28 23:53:19 +0530

    Initial commit AOP Implementation

---
+++ src/main/java/com/custom/aopimpl/AopCrudImplApplication.java
@@ -0,0 +1,13 @@
+package com.custom.aopimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopCrudImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopCrudImplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/custom/aopimpl/AopCrudImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.custom.aopimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopCrudImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
